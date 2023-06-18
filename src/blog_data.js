babe54117424dc4adcdd493d9b9343e11d3515b7
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-06-18 14:10:50 -0400

    @babel/plugin-proposal-private-property-in-object

--- src/blog_data.js
+++ src/blog_data.js
@@ -1,7 +1,7 @@
 const blog = [
     {
         id : 1,
-        title : 'FETCH Requests for Beginers',
+        title : 'FETCH Requests for Beginners',
         date : 'June 12, 2023',
         link : "https://dev.to/boucky/fetch-requests-for-beginners-35fn",
         image : 'https://res.cloudinary.com/practicaldev/image/fetch/s--yIzcbHBa--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/6vhs7whs1y597ks54a1x.jpg'
@@ -15,6 +15,20 @@ const blog = [
     },
     {
         id : 3,
+        title : 'FETCH Requests for Beginers',
+        date : 'June 12, 2023',
+        link : "https://dev.to/boucky/fetch-requests-for-beginners-35fn",
+        image : 'https://res.cloudinary.com/practicaldev/image/fetch/s--yIzcbHBa--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/6vhs7whs1y597ks54a1x.jpg'
+    },
+    {
+        id : 4,
+        title : 'FETCH Requests for Beginers',
+        date : 'June 12, 2023',
+        link : "https://dev.to/boucky/fetch-requests-for-beginners-35fn",
+        image : 'https://res.cloudinary.com/practicaldev/image/fetch/s--yIzcbHBa--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/6vhs7whs1y597ks54a1x.jpg'
+    },
+    {
+        id : 5,
         title : 'FETCH Requests for Beginers',
         date : 'June 12, 2023',
         link : "https://dev.to/boucky/fetch-requests-for-beginners-35fn",
