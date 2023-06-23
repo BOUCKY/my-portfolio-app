176e523f83db2888dc0b703ee11e6dfb6e6377ed
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-06-23 15:31:32 -0400

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,9 @@ import React from "react";
 const Project = ({image, title, link, date }) => {
     return(
         <div className="projectContent">
-            <img className="projectImage" src={image} alt="Project" />
+            <div className="projectImageContainer">
+                {/* <img className="projectImage" src={image} alt=" " /> */}
+            </div>
             <div className="projectText">
                 <a className='projectLink' href={link}>{title}</a>
                 <p>{date}</p>
