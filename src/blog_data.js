4840802a39acd722934d06c3e1e12f15e1767a2a
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-07-14 11:40:41 -0400

    Add Blog

--- src/blog_data.js
+++ src/blog_data.js
@@ -36,6 +36,24 @@ const blog = [
         preview: 'WHAT IS CSS? CSS, OR CASCADING STYLE SHEETS IS A FRONT-END LANGUAGE USED TO MAKE YOUR WEBSITE LOOK GOOD. WITHOUT CSS, YOUR WEBSITE WOULD . . .',
         link : "https://dev.to/boucky/these-simple-css-tricks-will-take-your-website-to-the-next-level-4neb",
         image : 'https://community-cdn-digitalocean-com.global.ssl.fastly.net/ghLpAM1gf6jm9AAqXekrZmip'
+    },
+    {
+        id : 5,
+        title : 'TOP-10 POST FOR FE (WEEK 1, JUL 2023)',
+        date : 'JULY 10, 2023',
+        minutes: '3 MINUTE READ',
+        preview: "THIS IS A WONDERFUL BLOG WITH THE TOP 10 FRONT-END RELATED BLOGS FROM THE FIRST WEEK OF JULY, FEATURING MY PREVIOUS BLOG: 'THESE SIMPLE CSS TRICKS WILL TAKE YOUR WEBSITE TO THE NEXT LEVEL'! WRITTEN BY FRUNTEND.",
+        link : "https://dev.to/fruntend/top-10-posts-for-fe-week-1-jul-2023-1931",
+        image : 'https://res.cloudinary.com/practicaldev/image/fetch/s--yAMo7-TF--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/h52yd0jql2w19ehaq8vl.jpg'
+    },
+    {
+        id : 6,
+        title : 'PYTHON BEST PRACTICES: A GUIDE TO WRITING CLEAN AND READABLE CODE',
+        date : 'JULY 14, 2023',
+        minutes: '4 MINUTE READ',
+        preview: 'THE ABILITY TO WRITE CLEAN AND READABLE CODE IS ESSENTIAL FOR NOT ONLY PYTHON DEVELOPERS, BUT ALL DEVELOPERS. HAVING CLEAN CODE . . .',
+        link : "https://dev.to/boucky/python-best-practices-a-guide-to-writing-clean-and-readable-code-25k4",
+        image : 'https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0349cd1nvtw9a2q39pde.png'
     }
 ]
 
