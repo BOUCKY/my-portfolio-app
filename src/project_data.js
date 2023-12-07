34a407dd2668252f546d7c426a35ae2dc39fe97b
BOUCKY <130799287+BOUCKY@users.noreply.github.com>
2023-12-07 13:08:42 -0500

    Update project_data.js

--- src/project_data.js
+++ src/project_data.js
@@ -18,7 +18,7 @@ const project = [
         id : 1,
         title : 'TASTY BITES',
         date : 'JUNE 29, 2023',
-        link : "https://tasty-bites.netlify.app/",
+        link : "https://youtu.be/b3e1xlt98xk",
         githubLink : "https://github.com/BOUCKY/Phase-2-Project",
         preview : 'A PLACE TO BROWSE RECIPES, ADD SOME TO YOUR FAVORITES, AND EVEN ADD YOUR OWN.',
         image : tastyBites
@@ -45,7 +45,7 @@ const project = [
         id : 4,
         title : 'CLI HANGMAN',
         date : 'JULY 21, 2023',
-        link : "https://github.com/BOUCKY/Project-3",
+        link : "https://youtu.be/ZRW7Rf-LeEg",
         githubLink : "https://github.com/BOUCKY/Project-3",
         preview : 'A FUN COMMAND LINE HANGMAN GAME!',
         image : Hangman
@@ -54,7 +54,7 @@ const project = [
         id : 5,
         title : 'MY TRAILS',
         date : 'AUGUST 11, 2023',
-        link : "https://my-trails.netlify.app/",
+        link : "https://youtu.be/ZVYZ9F0Z5VI",
         githubLink : "https://github.com/bonbongiraffe/mytrails",
         preview : 'A HIKING APP TO POST AND VIEW TRAILS.',
         image : myTrails
@@ -63,7 +63,7 @@ const project = [
         id : 6,
         title : "TRAVLER'S CLUB",
         date : 'AUGUST 25, 2023',
-        link : "https://github.com/BOUCKY/final_project",
+        link : "https://youtu.be/UlrMBCGLmno",
         githubLink : "https://github.com/BOUCKY/final_project",
         preview : 'BROWSE VARIOUS DESTINATIONS AND POST YOUR TRAVELS!',
         image : Travlers
