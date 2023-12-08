e377a73325c347aae30bbad222f6990518e7da2a
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-12-08 14:45:03 -0500

    Open resume in new tab.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from "react"
 import MyResume from '../AlexisBoucouvalasResume.pdf'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faDownload } from "@fortawesome/free-solid-svg-icons"
 
 const Resume = () => {
     return(
@@ -9,7 +9,7 @@ const Resume = () => {
             <button className="dropbtn">RESUME</button>
             <div className="dropdown-content">
                 <li>
-                    <a href={MyResume} target="_blank" rel="noreferrer">DOWNLOAD     <FontAwesomeIcon icon={faDownload} aria-hidden='true' /></a>
+                    <a href={MyResume}>DOWNLOAD     <FontAwesomeIcon icon={faDownload} aria-hidden='true' /></a>
                 </li>
             </div>
         </div>
