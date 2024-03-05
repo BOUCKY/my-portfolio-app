8ffdbb51c4c55ff233e625df90b04863e696842a
Alexis Boucouvalas <alexisbou16@gmail.com>
2024-03-05 18:31:17 -0500

    Fix popup

--- src/components/Design.js
+++ src/components/Design.js
@@ -12,19 +12,10 @@ const Design = ({ image, title, date, description }) => {
   }
 
   return (
-    <div>
-      {showPopup && (
-        <div className="popup" onClick={togglePopup}>
-          <div className="popupInner">
-            <img className="popupImage" src={image} alt="Design Popup" style={{ width: "50%" }} />
-            <button className="closeButton" onClick={togglePopup}>{close}</button>
-          </div>
-        </div>
-      )}
-      {!showPopup && (
+    <div className="designContainer">
         <div className="designContent">
             <div className="designImageContainer">
-                <img className="designImage" src={image} alt="Deisgn" onClick={togglePopup}/>
+                <img className="designImage" src={image} alt="Design" onClick={togglePopup}/>
             </div>
             <div className="designText">
                 <p className="designTitle">{title}</p>
@@ -32,9 +23,16 @@ const Design = ({ image, title, date, description }) => {
                 <p className="date">{date}</p>
             </div>
         </div>
-      )}
+        {showPopup && (
+            <div className="popup" onClick={togglePopup}>
+                <div className="popupInner">
+                    <img className="popupImage" src={image} alt="Design Popup" style={{ width: "50%" }} />
+                    <button className="closeButton" onClick={togglePopup}>{close}</button>
+                </div>
+            </div>
+        )}
     </div>
-  );
-};
+  )
+}
 
 export default Design;
