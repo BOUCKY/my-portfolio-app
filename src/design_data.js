743067dbbcb453b12b565e79b79374a83edef667
Alexis Boucouvalas <alexisbou16@gmail.com>
2024-03-05 14:01:56 -0500

    Description for designs.

--- src/design_data.js
+++ src/design_data.js
@@ -10,42 +10,42 @@ const design = [
         id: 1,
         title: 'STAMP #1: ART NOUVEAU',
         date: 'FEBRUARY 25, 2024',
-        description: 'description',
+        description: 'THE FIRST STAMP IN A SERIES OF SEVEN STAMPS, SHOWCASING MY IDEA OF WHAT THE AIR JORDAN LOGO WOULD LOOK LIKE THROUGH VARIOUS DESIGN AND ARTISTIC MOVEMENTS THROUGHOUT HISTORY.',
         image: artNouveau
     },
     {
         id: 2,
         title: 'STAMP #2: MODERNIST',
         date: 'MARCH 4, 2024',
-        description: 'description',
+        description: 'THE SECOND STAMP IN A SERIES OF SEVEN STAMPS, SHOWCASING MY IDEA OF WHAT THE AIR JORDAN LOGO WOULD LOOK LIKE THROUGH VARIOUS DESIGN AND ARTISTIC MOVEMENTS THROUGHOUT HISTORY.',
         image: modernist
     },
     {
         id: 3,
         title: 'PSYCHO MOVIE POSTER REDESIGN',
         date: 'APRIL 4, 2023',
-        description: 'description',
+        description: "A REVAMP OF THE ORIGINAL POSTER FOR ALFRED HITCHCOCK'S 'PSYCHO'.",
         image: psycho
     },
     {
         id: 4,
         title: 'ADICTION ADVOCACY POSTER',
         date: 'FEBRUARY 25, 2024',
-        description: 'description',
+        description: 'AN ADVOCACY POSTER FOCUSED ON THE STRUGGLES OF SUBSTANCE ABUSE. I AIMED TO ILLUSTRATE HOW ADDICTION CAN PREVENT INDIVIDUALS FROM EXPERIENCING BEAUTIFUL AND IMPORTANT MOMENTS IN THEIR LIVES.',
         image: advocacy
     },
     {
         id: 5,
         title: 'MAGAZINE SPREAD REDESIGN',
         date: 'APRIL 6, 2023',
-        description: 'description',
+        description: "A REDESIGN OF ALAN ALDA'S SPREAD 'ALAN ALDA IS OBSESSED WITH SCIENCE' IN AARP THE MAGAZINE JUNE/JULY 2020.",
         image: magazine
     },
     {
         id: 6,
         title: 'ISTVAN OROSZ TRADING CARD',
         date: 'MAY 4, 2023',
-        description: 'description',
+        description: "DRAWING INSPIRATION FROM ISTVAN OROSZ AND HIS WORK, I'VE CRAFTED A TRADING CARD IN HIS HONOR.",
         image: tradingCard
     },
 ]
